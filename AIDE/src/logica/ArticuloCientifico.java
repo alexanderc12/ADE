91a96c499a5364fc166c7a54a3e07b2700102164
Alexander <Alexander@192.168.1.7>
2015-03-23 18:30:51 -0500

    Se deja lista la logica de la extraccion de la informacion de un articulo.

--- AIDE/src/logica/ArticuloCientifico.java
+++ AIDE/src/logica/ArticuloCientifico.java
@@ -1,28 +1,66 @@
 package logica;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
+/**
+ * Representa la estructura de un articulo cientifico
+ * 
+ * @author Alexander Castro
+ *
+ */
 public class ArticuloCientifico {
-	
-	private ArrayList<String> listaAutores;
+
 	private LocalDate fechaRecepcion;
 	private LocalDate fechaAprobacion;
 	private int volumen;
 	private int numero;
 	private String titulo;
 	private String resumen;
+	private ArrayList<String> listaAutores;
 	private ArrayList<String> listaReferencias;
-	private ArrayList<String> palabrasClave;
-	private ArrayList<String> contenidoCapitulos;
-	
-	public ArticuloCientifico() {
+	private ArrayList<String> listaPalabrasClave;
+	private ArrayList<String> listaContenidoCapitulos;
+	private ArrayList<String> listaCapitulos;
+	private URL url;
+
+	public ArticuloCientifico(String url) {
+		try {
+			this.url = new URL(url);
+		} catch (MalformedURLException e) {
+			JOptionPane.showMessageDialog(null, LectorWebArticulo.ERROR_URL);
+		}
 		listaAutores = new ArrayList<String>();
 		listaReferencias = new ArrayList<String>();
-		palabrasClave = new ArrayList<String>();
-		contenidoCapitulos = new ArrayList<String>();
+		listaPalabrasClave = new ArrayList<String>();
+		listaContenidoCapitulos = new ArrayList<String>();
+		listaCapitulos = new ArrayList<String>();
 	}
-	
+
+	public void agregarAutor(String autor) {
+		listaAutores.add(autor);
+	}
+
+	public void agregarReferencia(String referencia) {
+		listaReferencias.add(referencia);
+	}
+
+	public void agregarPalabraClave(String palabra) {
+		listaPalabrasClave.add(palabra);
+	}
+
+	public void agregarCapitulo(String capitulo) {
+		listaCapitulos.add(capitulo);
+	}
+
+	public void agregarContenido(String contenido) {
+		listaContenidoCapitulos.add(contenido);
+	}
+
 	public ArrayList<String> getListaAutores() {
 		return listaAutores;
 	}
@@ -60,16 +98,16 @@ public class ArticuloCientifico {
 		this.listaReferencias = listaReferencias;
 	}
 	public ArrayList<String> getPalabrasClave() {
-		return palabrasClave;
+		return listaPalabrasClave;
 	}
 	public void setPalabrasClave(ArrayList<String> palabrasClave) {
-		this.palabrasClave = palabrasClave;
+		this.listaPalabrasClave = palabrasClave;
 	}
 	public ArrayList<String> getContenidoCapitulos() {
-		return contenidoCapitulos;
+		return listaContenidoCapitulos;
 	}
 	public void setContenidoCapitulos(ArrayList<String> contenidoCapitulos) {
-		this.contenidoCapitulos = contenidoCapitulos;
+		this.listaContenidoCapitulos = contenidoCapitulos;
 	}
 	public LocalDate getFechaRecepcion() {
 		return fechaRecepcion;
@@ -82,5 +120,31 @@ public class ArticuloCientifico {
 	}
 	public void setFechaAprobacion(LocalDate fechaAprobacion) {
 		this.fechaAprobacion = fechaAprobacion;
+	}
+	public ArrayList<String> getListaCapitulos() {
+		return listaCapitulos;
+	}
+	public void setListaCapitulos(ArrayList<String> listaCapitulos) {
+		this.listaCapitulos = listaCapitulos;
+	}
+
+	public URL getUlr() {
+		return url;
+	}
+
+	public void setUlr(URL ulr) {
+		this.url = ulr;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder articulo = new StringBuilder();
+		articulo.append("Titulo: ").append(titulo).append("\nURL: ").append(url).append("\nAutores: ")
+		.append(listaAutores).append("\nResumen: ").append(resumen).append("\nPalabras clave:")
+		.append(listaPalabrasClave).append("\nVolumen: ").append(volumen).append("\nNumero: ").append(numero)
+		.append("\nFecha recepción: ").append(fechaRecepcion).append("\nFecha aprobación: ")
+		.append("\nLista capitulos: ").append(listaCapitulos).append("\nLista contenidos: ")
+		.append(listaContenidoCapitulos).append("\nLista referencias: ").append(listaReferencias);
+		return articulo.toString();
 	}
 }
--- AIDE/src/logica/LectorWebArticulo.java
+++ AIDE/src/logica/LectorWebArticulo.java
@@ -6,13 +6,26 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Permite leer una pagina Web
+ * 
+ * @author Alexander Castro
+ *
+ */
 public class LectorWebArticulo {
-	
+
 	public static final String ERROR_URL = "Error al leer la URL del articulo.";
 	public static final String ERROR_CARGAR = "Error al cargar el articulo.";
-	public static final String ERROR_LECTURA = "Error al leer el articulo.";
+	public static final String ERROR_LECTURA = "Error al leer el texto del articulo.";
 	public static final String ERROR_CERRAR = "Error al cerrar el canal de datos.";
-	
+
+	/**
+	 * Apartir de un URL retorna el contenido HTML de un pagina
+	 *
+	 * @param urlArticulo
+	 * @return String con el HTML de la pagina
+	 */
+
 	public static String leerArticulo(String urlArticulo){
 		URL url;
 		try {
@@ -26,11 +39,11 @@ public class LectorWebArticulo {
 		} catch (IOException e) {
 			return ERROR_CARGAR;
 		}
-		String linea = "";
-		String texto = "";
+		String linea = null;
+		StringBuilder texto = new StringBuilder();
 		try {
 			while ((linea = entrada.readLine()) != null)
-				texto += linea + "\n";
+				texto.append(linea);
 		} catch (IOException e) {
 			return ERROR_LECTURA;
 		}
@@ -39,6 +52,24 @@ public class LectorWebArticulo {
 		} catch (IOException e) {
 			return ERROR_CERRAR;
 		}
-		return texto;
+		return remplazarCaracteresHTML(texto.toString());
 	}
+
+	/**
+	 * Resuelve problemas de internazionalización cambiando los caracteres
+	 * especiales de HTML a sus correspondientes valor para porder analizarlos
+	 * en JAVA
+	 *
+	 * @param texto
+	 *            con caracteres especiales de HTML
+	 * @return texto libre de caracteres especiales de HTML
+	 */
+	private static String remplazarCaracteresHTML(String texto) {
+		return texto = texto.replaceAll("&aacute;", "á").replace("&eacute;", "é").replace("&iacute;", "í")
+				.replace("&oacute;", "ó").replace("&uacute;", "ú").replace("&Aacute;", "Á").replace("&Eacute;", "É")
+				.replace("&Iacute;", "Í").replace("&Oacute;", "Ó").replace("&Uacute;", "Ú").replace("&ntilde;", "ñ")
+				.replace("&Ntilde;", "Ñ").replace("&amp;", "&").replace("&nbsp;", " ").replace("&#91;", "[")
+				.replace("&#93;", "]");
+	}
+
 }
