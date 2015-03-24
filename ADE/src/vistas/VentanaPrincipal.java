e8b08cc8793fbaac5446e889949cc769fddecf79
Alexander <Alexander@192.168.1.7>
2015-03-23 19:11:03 -0500

    Estructura afinda.

--- ADE/src/vistas/VentanaPrincipal.java
+++ ADE/src/vistas/VentanaPrincipal.java
@@ -10,6 +10,10 @@ import controladores.ControladorEventos;
 public class VentanaPrincipal extends JFrame {
 
 	public static final String E_CARGAR_WEB = "CARGAR_WEB";
+
+	public static final String T_TITULO = "ADE";
+	private static final int WIDTH = 500;
+	private static final int HEIGHT = 500;
 	/**
 	 *
 	 */
@@ -18,7 +22,8 @@ public class VentanaPrincipal extends JFrame {
 
 	public VentanaPrincipal() {
 		controladorEventos = new ControladorEventos(this);
-
+		setTitle(T_TITULO);
+		setSize(WIDTH, HEIGHT);
 	}
 
 	protected ImageIcon createImageIcon(String path, String description) {
@@ -31,7 +36,8 @@ public class VentanaPrincipal extends JFrame {
 	}
 
 	public static void main(String[] args) {
-
+		VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+		ventanaPrincipal.setVisible(true);
 	}
 
 }
