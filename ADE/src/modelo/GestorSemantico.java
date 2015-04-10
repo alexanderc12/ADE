2f1546dab95d3c70078172c3126c9254e39b7285
Alexander <Alexander@192.168.1.7>
2015-04-09 19:56:37 -0500

    Librerias organizadas y ya sirve lo de sinonimos.

---
+++ ADE/src/modelo/GestorSemantico.java
@@ -0,0 +1,119 @@
+package modelo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class GestorSemantico {
+
+	public void buscarSinonimos(String termino) {
+		InputStream in = null;
+		try {
+			in = new FileInputStream(new File("src/data/tesauro.rdf"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Model model = ModelFactory.createMemModelMaker().createDefaultModel();
+		model.read(in, null);
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String queryString = "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
+				+ " SELECT ?altLabel "
+				+ " WHERE {?concept skos:altLabel ?altLabel . ?concept skos:prefLabel ?prefLabel ."
+				+ " FILTER regex(?prefLabel, \""
+				+ termino + "\", \"i\") FILTER(lang(?altLabel) =\"es\")}";
+
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+		while (results.hasNext()) {
+			QuerySolution row = results.nextSolution();
+			System.out.println(row.getLiteral("altLabel").getString());
+		}
+		qe.close();
+	}
+
+	/**
+	 * narrower
+	 */
+	public void buscarAreasRelacionadas() {
+		InputStream in = null;
+		try {
+			in = new FileInputStream(new File("src/data/tesauro.rdf"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Model model = ModelFactory.createMemModelMaker().createDefaultModel();
+		model.read(in, null);
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String queryString = "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" + " SELECT ?altLabel "
+				+ " WHERE {?concept skos:altLabel ?altLabel . ?concept skos:prefLabel ?a .  ?concept"
+				+ " FILTER regex(?a, \"seguridad\", \"i\") FILTER(lang(?altLabel) =\"es\")}";
+
+		Query query = QueryFactory.create(queryString);
+
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+
+		ResultSetFormatter.out(System.out, results, query);
+
+		qe.close();
+	}
+
+	/**
+	 * broader
+	 */
+	public void buscarSubAreasRelacionadas() {
+		InputStream in = null;
+		try {
+			in = new FileInputStream(new File("src/data/tesauro.rdf"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Model model = ModelFactory.createMemModelMaker().createDefaultModel();
+		model.read(in, null);
+		try {
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String queryString = "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" + " SELECT ?altLabel "
+				+ " WHERE {?concept skos:altLabel ?altLabel . ?concept skos:prefLabel ?a ."
+				+ " FILTER regex(?a, \"seguridad\", \"i\") FILTER(lang(?altLabel) =\"es\")}";
+
+		Query query = QueryFactory.create(queryString);
+
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+
+		ResultSetFormatter.out(System.out, results, query);
+
+		qe.close();
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		GestorSemantico t = new GestorSemantico();
+		t.buscarSinonimos("docente");
+	}
+
+}
